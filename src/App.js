5e0fd32d53a3ad5fdced122f2a9109ed455ee85b
hwangs12 <sungjun94@gmail.com>
2021-09-09 22:45:34 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+import Photo from "./Photo";
+// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
+const clientId = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+
+function App() {
+	const [loading, setLoading] = useState(false);
+	const [photos, setPhotos] = useState([]);
+	const [page, setPage] = useState(1);
+	const [input, setInput] = useState("");
+	const [query, setQuery] = useState("red");
+	const [querySelected, setQuerySelected] = useState(false);
+
+	const fetchUrl = async () => {
+		setLoading(true);
+		let url;
+		const urlPage = `&page=${page}`;
+		const queryValue = `&query=${query}`;
+		url = querySelected
+			? `${searchUrl}${clientId}${queryValue}${urlPage}`
+			: `${mainUrl}${clientId}${urlPage}`;
+		try {
+			const res = await fetch(url);
+			const data = await res.json();
+			setPhotos((photos) => {
+				if (querySelected) {
+					return [...data.results];
+				}
+				return [...photos, ...data];
+			});
+			setLoading(false);
+		} catch (err) {
+			console.log(err);
+			setLoading(false);
+		}
+	};
+
+	const handleClick = (e) => {
+		e.preventDefault();
+		setQuerySelected(true);
+		setQuery(input);
+	};
+
+	useEffect(() => {
+		fetchUrl();
+	}, [page, query]);
+
+	useEffect(() => {
+		const event = window.addEventListener("scroll", () => {
+			if (
+				!loading &&
+				Math.abs(
+					window.innerHeight +
+						window.pageYOffset -
+						document.body.offsetHeight
+				) <= 2
+			) {
+				setPage((page) => page + 1);
+			}
+		});
+		return () => {
+			window.removeEventListener("scroll", event);
+		};
+	}, []);
+
+	return (
+		<main>
+			<section className="search">
+				<form className="search-form">
+					<input
+						type="text"
+						className="form-input"
+						placeholder="search"
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+					/>
+					<button
+						className="submit-btn"
+						type="submit"
+						onClick={handleClick}
+					>
+						<FaSearch />
+					</button>
+				</form>
+			</section>
+			<section className="photos">
+				<div className="photos-center">
+					{photos.map((photo) => {
+						return <Photo key={photo.id} {...photo} />;
+					})}
+				</div>
+				{loading && <h2 className="loading">Loading...</h2>}
+			</section>
+		</main>
+	);
+}
+
+export default App;
